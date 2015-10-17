f46cd3df527b8952791f719eb1b6119c0b884c54
Limian Wang <limian.wang@gmail.com>
2015-10-16 23:27:50 -0700

    config: setup brew env

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Ensure latest Homebrew is used
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade --all
+
+brew install bash
+brew install vim
+
+brew install ack
+brew install git
+brew install git-extras
+brew install go
+brew install nvm
+brew install redis
+brew install mongodb
+brew install mysql
+
+# Install Homebrew Caskroom
+brew install caskroom/cask/brew-cask
+brew cask install iterm2
+
+# Remove outdated versions from the cellar.
+brew cleanup -s
